9b0d0a3eb5e5a13230ef0087f1dd963089fa5f7c
Nikita Kochetov <nothsmple@mail.ru>
2021-06-22 21:15:30 +0300

    Initial Commit

---
+++ Guess It/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess It
+//
+//  Created by Татьяна Кочетова on 22.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
