48b53983346f393d5ec17a118c699835822305e9
Nikita Kochetov <nothsmple@mail.ru>
2021-06-25 11:20:21 +0300

    Timer has been added.

--- Guess It WatchKit Extension/InterfaceController.swift
+++ Guess It WatchKit Extension/InterfaceController.swift
@@ -17,6 +17,8 @@ class InterfaceController: WKInterfaceController {
     let movies = FilmModel.getFilms()
     var element: FilmModel?
     
+    private var timer: Timer?
+    
     
     override func awake(withContext context: Any?) {
         element = movies.randomElement()
@@ -26,6 +28,13 @@ class InterfaceController: WKInterfaceController {
     
     @IBAction func refreshAction() {
         myLabel.setText(element?.name)
+        
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.5, repeats: false, block: { (_) in
+            self.element = self.movies.randomElement()
+            self.myButton.setTitle(self.element?.emojis)
+            self.myLabel.setText("")
+        })
     }
     
 
